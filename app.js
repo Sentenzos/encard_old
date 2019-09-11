8e76a9073762ba73c4fdc4a54cd5d89e1053eb7f
Sentenzos <49534576+Sentenzos@users.noreply.github.com>
2019-09-11 22:29:18 +0300

    закомментил логгер

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.engine('ejs', require('ejs-locals')); // ejs-locals это сторонни�
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
